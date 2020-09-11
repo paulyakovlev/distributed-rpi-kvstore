eab0c96b5aba3dd07175b11c8bba2a80d7e28b02
Paul Yakovlev <yvpavel@gmail.com>
2020-09-11 00:26:29 -0700

    fix imports

--- run.py
+++ run.py
@@ -5,5 +5,4 @@ from os import environ
 if __name__ == '__main__':
     app = Flask(__name__)
     api.init_app(app)
-    app.config['FORWARDING_ADDRESS'] = environ.get('FORWARDING_ADDRESS')
     app.run(host='0.0.0.0', debug=True, port=8085)
