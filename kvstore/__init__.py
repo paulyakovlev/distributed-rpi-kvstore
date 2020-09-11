a68009bc4a6f173922bed87ea56da2770a28d83c
Paul Yakovlev <yvpavel@gmail.com>
2020-09-10 23:47:45 -0700

    update routes init.py

--- kvstore/__init__.py
+++ kvstore/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_restx import Api
 from os import environ
 
 app = Flask(__name__)
 app.config['FORWARDING_ADDRESS'] = environ.get('FORWARDING_ADDRESS')
+api = Api(app, title='Distributed KV Store', description='asdf')
 
 from kvstore import routes
+ 
