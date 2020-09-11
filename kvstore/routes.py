f24521c71d515c0575e633aa8b5b5c813c3b94c6
Paul Yakovlev <yvpavel@gmail.com>
2020-09-10 17:03:31 -0700

    migrate to restx

--- kvstore/routes.py
+++ kvstore/routes.py
@@ -1,7 +1,8 @@
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
+from flask_restx import Resource
 from functools import wraps
-from kvstore import app
+from kvstore import api, app
 import os
 import requests
 
@@ -50,71 +51,68 @@ def forward_request(f):
                     'error': 'Main instance is down',  'message': 'Error in %s' % request.method}
                 status_code = 503
         
-            return jsonify(response), status_code
+            return make_response(jsonify(response), status_code)
         else:
             return f(*args, **kwargs)
     return decorated_function
 
 # /key-value-store GET request
-@app.route('/key-value-store/<key>', methods=['GET'])
-@forward_request
-def get_key(key):
-    print('Main container received %s' % request.method, flush=True)
+@api.route('/key-value-store/<string:key>')
+class KeyStore(Resource):
+    @forward_request
+    def get(self,key):
+        print('Main container received %s' % request.method, flush=True)
 
-    if(key in key_store):
-        response = {
-            'doesExist': True, 'message': 'Retrieved successfully', 'value': key_store[key]}
-        status_code = 200
-    else:
-        response = {
-            'doesExist': False, 'error': 'Key does not exist', 'message': 'Error in GET'}
-        status_code = 404
-            
-    return jsonify(response), status_code
-
-# /key-value-store PUT request
-@app.route('/key-value-store/<key>', methods=['PUT'])
-@forward_request
-def add_or_update_key(key):
-    print('Main container received %s' % request.method, flush=True)
-
-    if('value' in request.json):
-        if (len(key) > 50):
+        if(key in key_store):
             response = {
-                'error': 'Key is too long', 'message': 'Error in PUT'}
-            status_code = 400
+                'doesExist': True, 'message': 'Retrieved successfully', 'value': key_store[key]}
+            status_code = 200
         else:
-            if (key in key_store):
-                key_store[key] = request.json['value']
+            response = {
+                'doesExist': False, 'error': 'Key does not exist', 'message': 'Error in GET'}
+            status_code = 404
+                
+        return make_response(jsonify(response), status_code)
+
+    @forward_request
+    def put(self,key):
+        print('Main container received %s' % request.method, flush=True)
+
+        if('value' in request.json):
+            if (len(key) > 50):
                 response = {
-                    'message': 'Updated successfully', 'replaced': True}
-                status_code = 200
+                    'error': 'Key is too long', 'message': 'Error in PUT'}
+                status_code = 400
             else:
-                key_store[key] = request.json['value']
-                response = {'message': 'Added successfully',
-                            'replaced': False}
-                status_code = 201
-    else:
-        response = {'error': 'Value is missing',
-                    'message': "Error in PUT"}
-        status_code = 400
-            
-    return jsonify(response), status_code
+                if (key in key_store):
+                    key_store[key] = request.json['value']
+                    response = {
+                        'message': 'Updated successfully', 'replaced': True}
+                    status_code = 200
+                else:
+                    key_store[key] = request.json['value']
+                    response = {'message': 'Added successfully',
+                                'replaced': False}
+                    status_code = 201
+        else:
+            response = {'error': 'Value is missing',
+                        'message': "Error in PUT"}
+            status_code = 400
+                
+        return make_response(jsonify(response), status_code)
 
-# /key-value-store DELETE request
-@app.route('/key-value-store/<key>', methods=['DELETE'])
-@forward_request
-def delete_key(key):
-    print('Main container received %s' % request.method, flush=True)
+    @forward_request
+    def delete(self,key):
+        print('Main container received %s' % request.method, flush=True)
 
-    if (key in key_store):
-        del key_store[key]
-        response = {
-            'doesExist': True, 'message': 'Deleted successfully'}
-        status_code = 200
-    else:
-        response = {
-            'doesExist': False, 'error': 'Key does not exist', 'message': 'Error in DELETE'}
-        status_code = 404
-            
-    return jsonify(response), status_code
+        if (key in key_store):
+            del key_store[key]
+            response = {
+                'doesExist': True, 'message': 'Deleted successfully'}
+            status_code = 200
+        else:
+            response = {
+                'doesExist': False, 'error': 'Key does not exist', 'message': 'Error in DELETE'}
+            status_code = 404
+                
+        return make_response(jsonify(response), status_code)
